13547229168ef250f63f095185c46bfbac657cb9
Indykatz <cats83@me.com>
2022-07-04 11:37:29 +0100

    first commit 01

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 // port const
 const port = process.env.PORT || 5001;
-// telling app to use the public folder, when a request is made to /stati
+// telling app to use the public folder, when a request is made to /static
 app.use("/static", express.static("public"));
 // listening on localhost:5001 for requests to our webserver
 app.listen(port, () => {
